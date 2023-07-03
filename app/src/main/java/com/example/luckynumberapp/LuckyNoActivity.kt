f4fa5f8669ecbaeaad12ebae9d70e7f2e2b66357
Karan Markandey <karan.iitg54@gmail.com>
2023-07-03 15:28:00 +0530

    App project completed

--- app/src/main/java/com/example/luckynumberapp/LuckyNoActivity.kt
+++ app/src/main/java/com/example/luckynumberapp/LuckyNoActivity.kt
@@ -1,11 +1,40 @@
 package com.example.luckynumberapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import androidx.databinding.DataBindingUtil
+import com.example.luckynumberapp.databinding.ActivityLuckyNoBinding
+import java.util.Random
+
 class LuckyNoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLuckyNoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lucky_no)
+        //setContentView(R.layout.activity_lucky_no)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_lucky_no)
+        binding.button2.setTextColor(resources.getColorStateList(R.color.white,null))
+        val bundle = intent.extras
+        val userName = bundle?.getString("USERNAME")
+        binding.textView3.text = "$userName, you are lucky today"
+        val luckyNumber =getTheLuckyNumber()
+         binding.textView4.text = "And your lucky number is ${luckyNumber}"
+        binding.button2.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            //intent.putExtra(Intent.EXTRA_SUBJECT ,"$userName, is lucky today" )
+            intent.putExtra(Intent.EXTRA_TEXT,"$userName, is lucky today\n with lucky number: $luckyNumber")
+            intent.setType("text/plain")
+            startActivity(intent)
+        }
+
+    }
+
+
+    private fun getTheLuckyNumber():Int{
+        val luckyNumber = Random().nextInt(100)
+        return luckyNumber
     }
 }
--- app/src/main/java/com/example/luckynumberapp/MainActivity.kt
+++ app/src/main/java/com/example/luckynumberapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.luckynumberapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.luckynumberapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.button.setOnClickListener {
+            var userName =  binding.editTextText.text.toString()
+            if(userName.isNotEmpty()){
+                val intent = Intent (this,LuckyNoActivity::class.java)
+                intent.putExtra("USERNAME",userName)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this,"Enter Username...",Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
