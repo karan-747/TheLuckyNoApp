74a9a791177be41ac22bf39572190612f2bc11ef
Karan Markandey <karan.iitg54@gmail.com>
2023-07-03 08:17:41 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/luckynumberapp/LuckyNoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.luckynumberapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LuckyNoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lucky_no)
+    }
+}
